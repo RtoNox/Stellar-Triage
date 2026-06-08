36e76feacb3d3fcfa5bb3acbdd7f1f5118db6bc0
Sebastian <thorncrepeer@gmail.com>
2026-06-08 18:03:39 +0700

    main menu manager semi working

--- Assets/Scripts/Main Menu Manager.cs
+++ Assets/Scripts/Main Menu Manager.cs
@@ -3,35 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public enum GameState
+{
+    MainMenu,
+    SaveFiles,
+    Settings
+}
+
 public class MainMenuManager : MonoBehaviour
 {
-    public GameObject settings;
-    public GameObject mainMenu;
-    public GameObject saveFiles;
+    public GameObject settingsUI;
+    public GameObject mainMenuUI;
+    public GameObject saveFilesUI;
 
-    public void OpenSave()
+    public static MainMenuManager instance;
+    public GameState currentState { get; private set; }
+
+    public void ChangeState(GameState newState)
     {
-        saveFiles.SetActive(true);
-        mainMenu.SetActive(false);
+        StartCoroutine(TransitionToState(newState));
+
+        currentState = newState;
+    }
+    
+    public void ChangeToSaveFiles()
+    {
+        ChangeState(GameState.SaveFiles);
     }
 
-    public void CloseSave()
+    public void ChangeToMainMenu()
     {
-        saveFiles.SetActive(false);
-        mainMenu.SetActive(true);
+        ChangeState(GameState.MainMenu);
     }
 
-    public void OpenSettings()
+    public void ChangeToSettings()
     {
-        settings.SetActive(true);
-        mainMenu.SetActive(false);
+        ChangeState(GameState.Settings);
     }
 
-    public void CloseSettings()
+    private IEnumerator TransitionToState(GameState newState)
     {
-        settings.SetActive(false);
-        mainMenu.SetActive(true);
+        if(newState != GameState.MainMenu)
+        {
+            yield return new WaitForSecondsRealtime(0);
+        }
+
+        currentState = newState;
+        HandleStateChange();
     }
+    
+    private void HandleStateChange()
+    {
+        HideAllMenu();
+
+        switch (currentState)
+        {
+            case GameState.MainMenu:
+                mainMenuUI.SetActive(true);
+                break;
+            case GameState.Settings:
+                settingsUI.SetActive(true);
+                break;
+            case GameState.SaveFiles:
+                saveFilesUI.SetActive(true);
+                break;
+        }
+    }
+
+    
+    private void HideAllMenu()
+    {
+        mainMenuUI.SetActive(false);
+        settingsUI.SetActive(false);
+        saveFilesUI.SetActive(false);
+    }
+
 
     public void QuitGame()
     {
