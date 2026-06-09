eece6e5adb76bd36f468b2128e3adf2b780e1979
Sebastian <thorncrepeer@gmail.com>
2026-06-09 19:01:51 +0700

    clock work day no

--- Assets/Scripts/ClockController.cs
+++ Assets/Scripts/ClockController.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ClockController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float elapsedTime;
+    private float timeInADay = 86400f;
+    public int dayCount = 1;
+    [SerializeField] private TMP_Text day;
+    [SerializeField] private TMP_Text time;
+    [SerializeField] private float timeScale = 60.0f;
+
+    private void Update()
     {
-        
+        elapsedTime += Time.deltaTime * timeScale;
+        elapsedTime %= timeInADay;
+        UpdateClockUI();
+        UpdateDay();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateClockUI()
     {
-        
+        int hours = Mathf.FloorToInt(elapsedTime / 3600f);
+        int minutes = Mathf.FloorToInt((elapsedTime - hours * 3600f) / 60f);
+        int seconds = Mathf.FloorToInt((elapsedTime - hours * 3600f) - (minutes * 60f));
+
+        string clockString = string.Format("{0:00}:{1:00}",hours, minutes);
+        time.text = clockString;
+    }
+
+    void UpdateDay()
+    {
+        if (elapsedTime >= timeInADay)
+        {
+        dayCount += 1;
+        }
+        day.text = dayCount.ToString();
     }
 }
