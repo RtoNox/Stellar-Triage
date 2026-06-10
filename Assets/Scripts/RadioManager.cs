52bf63c20d2b22c3aa4576f08c23d4c3a174466c
Sebastian <thorncrepeer@gmail.com>
2026-06-10 17:15:16 +0700

    radio randomizer (unfinished)

--- Assets/Scripts/RadioManager.cs
+++ Assets/Scripts/RadioManager.cs
@@ -2,11 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine;
 
 public class Radiomanager : MonoBehaviour
 {
     public UnityEvent OnClickInteraction;
     private SpriteRenderer spriteRenderer;
+    [SerializeField] private GameObject Radio;
+    private PolygonCollider2D polyCollider;
+
+    private float fiveSecondCheck;
+    private float fiveSecond = 5.0f;
+
+    private void Start()
+    {
+        polyCollider = Radio.GetComponent<PolygonCollider2D>();
+    }
+
+    private void Update()
+    {
+        float previousTime = fiveSecondCheck;
+        fiveSecondCheck += Time.deltaTime;
+        
+        if (previousTime < fiveSecond && fiveSecondCheck >= fiveSecond)
+        {
+            int eventIndex = Random.Range(1, 3);
+            if (eventIndex == 1)
+            {
+                if (polyCollider.enabled == false)
+                {
+                    Debug.Log($"roll 1");
+                    polyCollider.enabled = !polyCollider.enabled;
+                }
+            }
+            else
+            {
+                Debug.Log($"roll 2");
+            }
+
+        }
+        fiveSecondCheck %= fiveSecond;
+    }
 
     private void OnMouseDown()
     {
@@ -16,6 +52,7 @@ public class Radiomanager : MonoBehaviour
     public void TriggerInteraction()
     {
         Debug.Log($"radio click");
+        polyCollider.enabled = !polyCollider.enabled;
         OnClickInteraction?.Invoke(); 
     }
 }
